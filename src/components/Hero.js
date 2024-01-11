ef9dbefe5e496e011580f3d1ad4d45510560e546
islem <islembaaziz@yahoo.fr>
2024-01-11 20:17:43 +0100

    responsive fixing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,28 +7,27 @@ const Hero = () => {
     <div>
       <div className="container mx-auto flex flex-col md:flex-row py-2">
         <div className="w-full md:w-1/2 p-8 py-16">
-        
-          <h3 className="text-black text-lg text-left ml-8 font-bold">
+          <h3 className="text-black text-lg md:text-left md:ml-8 font-bold">
             Plan your trip now
           </h3>
-          <h1 className="text-6xl font-bold mt-2 text-left ml-8 py-4">
+          <h1 className="text-6xl font-bold mt-2 md:text-left md:ml-8 py-4">
             Save <span className="text-orange">big</span> with our car rental
           </h1>
-          <p className="text-gray-dark mt-4 text-left ml-8">
+          <p className="text-gray-dark mt-4 md:text-left md:ml-8">
             Rent the car of your dreams. Unbeatable prices, unlimited miles,
             flexible pick-up options and much more.
           </p>
-          <div className="mt-8 flex flex-col md:flex-row md:items-center ml-8">
-            <button className="bg-orange text-white rounded px-8 py-4 font-bold flex items-center mb-4 md:mb-0 md:mr-4">
+          <div className="mt-8 flex flex-col items-center md:flex-row md:items-center md:ml-8">
+            <button className="bg-orange text-white rounded px-8 py-4 flex items-center mb-4 md:mb-0 md:mr-4">
               Book Ride <CheckCircleOutlined className="ml-2" />
             </button>
-            <button className="bg-black text-white rounded px-8 py-4 font-bold flex items-center">
+            <button className="bg-black text-white rounded px-8 py-4 flex items-center">
               Learn More <DownOutlined className="ml-2" />
             </button>
           </div>
         </div>
-        <div className=" hidden md:block ">  
-        <div className="relative -z-50">
+        <div className=" hidden md:block ">
+          <div className="relative -z-50">
             <div className="absolute top-0 left-20 w-80 h-80 bg-red-300 rounded-full mix-blend-multiply filter blur-xl opacity-90 animate-blob"></div>
             <div className="absolute top-0 right-10 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
             <div className="absolute -top-10 right-20 w-80 h-80 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-3000"></div>
